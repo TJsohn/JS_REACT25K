99489e997b78675b7246defedb7260c23e485918
TJsohn <sohntaejeong@gmail.com>
2025-03-24 07:12:13 +0200

    remove comments

--- week_02/basic_functions/basic_04.js
+++ week_02/basic_functions/basic_04.js
@@ -7,10 +7,6 @@ Print the result using console.log().
 Hint: Use str[0].toUpperCase() and str.slice(1).toLowerCase().
 
 */
-/* function capitalize(str) {
-  return str[0].toUpperCase() + str.slice(1).toLowerCase();
-}
-console.log(capitalize("javascript"));*/
 
 const capitalize = (str) => {
   return str[0].toUpperCase() + str.slice(1).toLowerCase();
