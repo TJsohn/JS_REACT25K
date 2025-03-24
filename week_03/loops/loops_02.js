d01a2b8540079a27ad0eeba0b0bd781363e9a066
TJsohn <sohntaejeong@gmail.com>
2025-03-24 08:53:30 +0200

    leave the best solution

--- week_03/loops/loops_02.js
+++ week_03/loops/loops_02.js
@@ -3,43 +3,14 @@ Write a function named logEvenNumbers:
 - Use a loop to display all even numbers less than 100 in the pattern: 2, 98, 4, 96, ..., 50.
 Display all numbers in a single line in the console.; 
 */
-/*
-const logEvenNumbers = () => {
-  for (i = 2; i < 100; i++) {
-    if (i % 2 === 0) {
-      console.log(i, 100 - i);
-    }
-    i++;
-  }
-};*/
 
-const logEvenNumbers = () => {
+function logEvenNumbers() {
   let result = "";
   for (let i = 2, j = 98; i < j; i += 2, j -= 2) {
     result += `${i}, ${j}, `;
   }
   result += "50";
   console.log(result);
-};
+}
 
 logEvenNumbers();
-
-/* logEvenNumbers();
-
-const logNumbersOnetoFive = () => {
-  for (i = 1; i <= 5; i++) {
-    console.log(i);
-  }
-};
-
-logNumbersOnetoFive();
-
-const logNumbersOnetoFive2 = () => {
-  let i = 1;
-  while (i <= 5) {
-    console.log(i);
-    i++;
-  }
-};
-
-logNumbersOnetoFive2(); */
