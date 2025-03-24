ba45d3aa14b9d8175072955b7c3d6e2e32c89d69
TJsohn <sohntaejeong@gmail.com>
2025-03-24 07:13:23 +0200

    remove comments

--- week_02/basic_functions/basic_05.js
+++ week_02/basic_functions/basic_05.js
@@ -8,7 +8,6 @@ Hint: Use Math.random(), Math.floor(), and simple math.
 
 */
 const generateRandomNumber = (min, max) => {
-  //return Math.floor(Math.random(min) * max);
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
